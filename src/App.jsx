069d3c463960b41904ee77e0585512cb2a3fd23c
Aanand-code <aanandthakurr07@gmail.com>
2025-08-19 07:50:37 +0530

    Some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Layout from './layout/Layout.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
     <div className=" min-h-screen  w-full flex flex-col items-center">
       <div className="w-full lg:w-2xl min-h-screen flex flex-col p-8">
         <Layout />
+        <Analytics />
       </div>
     </div>
   );
